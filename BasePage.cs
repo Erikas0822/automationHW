bb6c6179c5141c2098b7f27eae6f05edde8fdfe1
Erikas0822 <89356839+Erikas0822@users.noreply.github.com>
2021-08-26 18:40:14 +0300

    Add files via upload

---
+++ BasePage.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+
+
+namespace AutomatinisReal.Page
+{
+    class BasePage
+    {
+            protected IWebDriver Driver;
+            public BasePage(IWebDriver webdriver)
+            {
+                Driver = webdriver;
+            }
+
+            public void CloseBrowser()
+            {
+                Driver.Quit();
+            }
+    }
+}
